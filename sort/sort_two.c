7792078f3f7566bda31eb4cade0e1af6e9b9c207
OUSSAMA ZAKKARE <oussama.zakkare@gmail.com>
2021-07-08 18:48:37 +0100

    norm (sort/sort_two.c)

--- sort/sort_two.c
+++ sort/sort_two.c
@@ -2,8 +2,9 @@
 
 int	sort_two(t_stack	*head, t_instructions **instrcs)
 {
-	int min;
-	int max;
+	int	min;
+	int	max;
+
 	if (stack_size(head) != 2)
 		return (0);
 	min = get_index_of_min(head);
@@ -18,16 +19,17 @@ int	sort_two(t_stack	*head, t_instructions **instrcs)
 
 int	unsort_two(t_stack	*head, t_instructions **instcs)
 {
-	int min;
-	int max;
+	int	min;
+	int	max;
+
 	if (stack_size(head) != 2)
 		return (0);
 	min = get_index_of_min(head);
 	if (min == 0)
 	{
 		swap(head);
-			add_node("sb", instcs);
-		return(1);
+		add_node("sb", instcs);
+		return (1);
 	}
 	return (1);
 }
